3b6fdbfe9118a726e8303700647578c27062e0b6
haginta <hagintagensbar191099@gmail.com>
2022-11-11 08:47:51 +0700

    perubahan 1

---
+++ app/src/main/java/com/example/belajarandroid2/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.belajarandroid2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+private lateinit var title:TextView
+private lateinit var edtWidth: EditText
+private lateinit var edtHeight: EditText
+private lateinit var edtLength: EditText
+private lateinit var btnCalculate:Button
+private lateinit var tvResult: TextView
+
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        title = findViewById(R.id.tv_title)
+        edtWidth = findViewById(R.id.edt_width)
+        edtHeight= findViewById(R.id.edt_height)
+        edtLength= findViewById(R.id.edt_length)
+        btnCalculate= findViewById(R.id.btn_calculate)
+        tvResult= findViewById(R.id.tv_result)
+
+        title.tex="hallo"
+        btnCalculate.setOnClickListener(this)
+        if (savedInstanceState != null) {
+            val result = savedInstanceState.getString("STATE_RESULT")
+            tvResult.text = result
+        }
+
+
+    }
+
+    override fun onClick(v: View?) {
+        if(v?.id == R.id.btn_calculate){
+            val inputLength=edtLength.text.toString().trim()
+            val inputWidth=edtWidth.text.toString().trim()
+            val inputHeight =edtHeight.text.toString().trim()
+            var isEmptyFields=false
+
+            if(inputLength.isEmpty()){
+                isEmptyFields=true
+                edtLength.error="isi dulu!!"
+            }
+            if(inputWidth.isEmpty()){
+                isEmptyFields=true
+                edtWidth.error="isi dulu!!"
+            }
+            if(inputHeight.isEmpty()){
+                isEmptyFields=true
+                edtHeight.error="isi dulu!!"
+            }
+            if(!isEmptyFields){
+                val volume=inputLength.toDouble()*inputWidth.toDouble()* inputHeight.toDouble()
+                tvResult.text=volume.toString()
+            }
+
+
+
+
+        }
+
+
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("STATE_RESULT", tvResult.text.toString())
+    }
+}
